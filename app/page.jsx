a79c1299e42ec9b49198dee4e7a5df7ea02bfa5a
timothymichaelsnyders-softengine <timothymichaelsnyders93@gmail.com>
2026-07-06 18:55:38 +0200

    Change logo image source to price-pulse-orange
    Updated logo image source in the header.

--- app/page.jsx
+++ app/page.jsx
@@ -44,8 +44,8 @@ export default async function Home() {
         <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
           {/* LOGO */}
           <div className="flex items-center gap-3">
-            <Image src={"/deal-drop-logo.png"} alt="Deal Drop Logo" 
-            // <Image src={"/price-pulse-orange.png"} alt="Deal Drop Logo" 
+            {/* <Image src={"/deal-drop-logo.png"} alt="Deal Drop Logo"  */}
+            <Image src={"/price-pulse-orange.png"} alt="Deal Drop Logo" 
               width={600}
               height={200}
               className="h-12 w-auto"
